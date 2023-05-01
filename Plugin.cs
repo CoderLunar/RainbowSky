d63763db1a5747f4de81513e53a01305829ce7bd
Lunar <120229100+CoderLunar@users.noreply.github.com>
2023-05-01 06:58:14 -0600

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaTagModTemplateProject
+{
+
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		GameObject Sky;
+		GameObject Monkey;
+		float monkR;
+		float monkG;
+		float monkB;
+
+		void Start()
+		{
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			Sky = GameObject.Find("/Level/newsky");
+			Monkey = GameObject.Find("Global/Local VRRig/Local Gorilla Player/gorilla");
+
+			var monkeyColor = Monkey.GetComponent<SkinnedMeshRenderer>().material.color;
+			monkR = monkeyColor.r;
+			monkG = monkeyColor.g;
+			monkB = monkeyColor.b;
+
+		}
+
+		void Update()
+		{
+			if(Sky != enabled)
+			{
+				Sky.SetActive(this.enabled);
+			}
+
+			Sky.GetComponent<MeshRenderer>().material = null;
+			Sky.GetComponent<MeshRenderer>().material.color = new Color(monkR, monkG, monkB);
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.lunar.gorillatag.rainbowsky";
+		public const string Name = "rainbowsky";
+		public const string Version = "1.0.0";
+	}
+}
